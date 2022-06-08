84f13a58fcd03163007c9c9c0e4c44a0aacaa87c
Felipe Curcio <felipe_nieto010@hotmail.com>
2022-06-08 15:39:33 -0300

    SOLID introduction

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import express from "express";
+import express from 'express';
 
-import { usersRoutes } from "./routes/users.routes";
+import { usersRoutes } from './routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/users", usersRoutes);
+app.use('/users', usersRoutes);
 
 export { app };
+
+// S - Single-responsiblity Principle
+// O - Open-closed Principle
+// L - Liskov Substitution Principle
+// I - Interface Segregation Principle
+// D - Dependency Inversion Principle
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,21 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
-
+import { ListAllUsersUseCase } from './ListAllUsersUseCase';
+import { IncomingHttpHeaders } from 'node:http';
+interface IRequestHeader extends IncomingHttpHeaders {
+  user_id: any;
+}
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers as IRequestHeader;
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
